fbf0fe73888cdfa7c4cc1de42255ce912f169734
Moses-Ongwenyi <moses.ongwenyi@ibm.com>
2020-05-01 14:30:38 +0300

    #6 posting a tag using the TagSerializer.

--- src/organizer/views.py
+++ src/organizer/views.py
@@ -4,6 +4,11 @@ from rest_framework.generics import (
     ListAPIView,
     RetrieveAPIView
 )
+from rest_framework.response import Response
+from rest_framework.status import (
+    HTTP_201_CREATED,
+    HTTP_400_BAD_REQUEST
+)
 
 from organizer.models import NewsLink, Startup, Tag
 from organizer.serializers import (
@@ -18,6 +23,20 @@ class TagAPIList(ListAPIView):
 
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+
+    def post(self, request):
+        """Create a new Tag."""
+        s_tag = self.serializer_class(
+            data=request.data,
+            context={'request': request}
+        )
+
+        if s_tag.is_valid():
+            s_tag.save()
+            return Response(
+                s_tag.data, status=HTTP_201_CREATED
+            )
+        return Response(s_tag.errors, status=HTTP_400_BAD_REQUEST)
 
 
 class TagAPIDetail(RetrieveAPIView):
